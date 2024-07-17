1d250db4b168886aa0d10d0e96eee068d2627da6
Davitmach <wvime30@gmail.com>
2024-07-17 16:08:32 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyapi.online/api/movies');
+        const data = await response.json();
+        setMovies(data); // сохраняем данные в состоянии
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // пустой массив зависимостей, чтобы useEffect вызывался только один раз
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.map((movie, index) => (
+        <div className='Movie_box'>
+          <div className='Info_box'>
+
+            <div className='Title'><h1>{movie.movie.length > 16 ? movie.movie.substring(0,16)+ '...':movie.movie}</h1></div>
+            <div className='Rating'><span>{movie.rating}</span></div>
+            <div><a href={movie.imdb_url}>View</a></div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
